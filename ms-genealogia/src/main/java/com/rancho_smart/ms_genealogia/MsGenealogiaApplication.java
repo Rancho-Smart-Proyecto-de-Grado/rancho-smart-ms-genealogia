b4795cd5c95ffcea88c626dc11a8a09e53e2bbe1
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 03:00:30 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-genealogia/src/main/java/com/rancho_smart/ms_genealogia/MsGenealogiaApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_genealogia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGenealogiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGenealogiaApplication.class, args);
+	}
+
+}
---
+++ ms-genealogia/src/test/java/com/rancho_smart/ms_genealogia/MsGenealogiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_genealogia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGenealogiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
